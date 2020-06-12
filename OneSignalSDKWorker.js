9f6d0c1be1a89f8d727ef26dda22fb41f8cdf3b4
LadyNewtonOficial <60409632+LadyNewtonOficial@users.noreply.github.com>
2020-06-12 10:40:23 -0300

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,2 +1,2 @@
-importScripts('https://https://ladynewtonoficial.github.io/mobirise/sw.js');
+importScripts('https://ladynewton.com/sw.js');
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
